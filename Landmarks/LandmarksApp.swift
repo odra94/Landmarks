cda7ad3fe20773c3ab6fbb8b8b3d203bcd58dba2
Oscar Reyes <odreyes94@gmail.com>
2024-02-13 21:00:48 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Oscar Reyes on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
